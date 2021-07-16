46ad3d4b49aaff09f8077eaa727b18c9b41f1fa8
Indiana Lora <indiana@ensaludybelleza.com>
2021-07-16 17:27:58 -0400

    created about section

--- src/component/about/about.js
+++ src/component/about/about.js
@@ -1,12 +1,25 @@
 import React from "react";
-import "./about.css"
+import SpellcheckIcon from "@material-ui/icons/Spellcheck";
+import GTranslateIcon from "@material-ui/icons/GTranslate";
+import ClassIcon from "@material-ui/icons/Class";
+import "./about.css";
 
 function About() {
-    return (
-        <div className="about" id="about">
-          
+  return (
+    <div className="about" id="about">
+      <div className="about-container">
+        <div className="bg-100">
+          <SpellcheckIcon className="about-icon" />
         </div>
-    )
+        <div className="bg-200">
+          <GTranslateIcon className="about-icon" />
+        </div>
+        <div className="bg-300">
+          <ClassIcon className="about-icon" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
