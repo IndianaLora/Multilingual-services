2c94d945a0f2ff5d30c093aeac99f49a9e124ad9
Indiana Lora <indiana@ensaludybelleza.com>
2021-08-10 18:24:40 -0400

    home section finish

--- src/component/contact/contact.js
+++ src/component/contact/contact.js
@@ -1,10 +1,27 @@
 import React from "react";
 import "./contact.css";
+import PhoneIcon from "@material-ui/icons/Phone";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
 
 function Contact() {
   return (
-    <div className="contact" id="contact">
-      <h1>Hey</h1>
+    <div className="contact-wrapper " id="contact">
+      <h1 className="text-center">Get in touch with us</h1>
+      <PhoneIcon className="contact-icon" />
+      <MailOutlineIcon className="contact-icon" />
+      <div className="contact">
+        <div className="contact-form" id="contact">
+          <input type="text" placeholder="Enter your name" />
+          <input type="text" placeholder="Enter your email" />
+          <textarea placeholder="Write your message please"></textarea>
+          <button>Submit</button>
+        </div>
+        <div className="contact-info" id="contact">
+          <h1>Contact us</h1>
+          <h2>1(809)456-6544</h2>
+          <h2>1(829)931-6544</h2>
+        </div>
+      </div>
     </div>
   );
 }
